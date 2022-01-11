bb7255af101a0ba86c966d034f5d69db644ec10f
Jorge Luis Vera <veravelis3@gmail.com>
2022-01-11 15:00:07 -0300

    Funcionando serve

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express();
 const mysql = require('mysql')
 
@@ -9,11 +11,20 @@ const mysql = require('mysql')
    database: 'cruddatabase',
  });
 
-app.get("/", (req, res) => {
-  const sqlInsert = "INSERT INTO ports (nombre, tipo) VALUES ('andres', 'conteo');"
-  db.query(sqlInsert, (err, result) => {
-    res.send("Hello Jorge Luis");
-  })
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true})); 
+
+app.post("/api/insert", (req, res) => {
+
+  const movieName = req.body.movieName
+  const movieReview = req.body.movieReview
+  
+  const sqlInsert = 
+  "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?)";
+  db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+    console.log(err);
+  });
 });
 
 
